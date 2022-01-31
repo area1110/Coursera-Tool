2213f9a4bb236d266073c4fa119db87b6ed99d06
area1110 <97070827+nguyenkhanhhuyhe153585@users.noreply.github.com>
2022-02-01 01:12:30 +0700

    ádf

--- CourseraTool+/background.js
+++
@@ -1,7 +0,0 @@
-let isChoosingMax = true;
-let isAutomatic = true;
-
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ isChoosingMax });
-  chrome.storage.sync.set({ isAutomatic });
-});
